7a66e8a5a3c3bffd3b3f6355061eb0554984a0b3
anshulbing <sansh786.as@gmail.com>
2023-08-10 15:47:44 +0530

    3first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('{"response: "Hello from HOMEPAGE"}');
+});
+
+app.get('/app1', function(req, res){
+    res.send('{"response: "Hello from APP-1"}');
+});
+
+app.get('/app2', function(req, res){
+    res.send('{"response: "Hello from APP-2"}');
+});
+
+app.listen(process.env.PORT || 3000);
+module.exports = app;
